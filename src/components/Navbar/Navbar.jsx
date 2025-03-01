6e63f8a73adbf18718f1f2b5422dfe8eb043cd5c
ubaidmalik-dev <ubaidmalikvk@gmail.com>
2025-03-01 13:35:53 +0500

    Update Navbar.jsx
    initial+2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom"; // Import useLocation
 import DarkLogo from "../../assets/logo.png";
-import LightLogo from "../../assets/Logo.png";
+import LightLogo from "../../assets/logo.png";
 import { FaCartShopping } from "react-icons/fa6";
 import { IoClose, IoMenu } from "react-icons/io5";
 
